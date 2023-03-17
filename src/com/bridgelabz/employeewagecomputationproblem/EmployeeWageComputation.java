7b0bf777c7ff9037bcdb5556c96a2c400eb25018
shephali09 <hiremathshephali9@gmail.com>
2023-03-17 22:26:30 +0530

    Add: Calculating wages for month. Assume 20 working days per month

--- src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
@@ -1,13 +1,14 @@
 package com.bridgelabz.employeewagecomputationproblem;
 
-/*Calculation Employee wage using
-  switch case statement */
+/*Calculating Wages for a Month assuming 20
+  Working Days in a Month */
  
 public class EmployeeWageComputation {
 	
 	public static final int is_part_time=1;
 	public static final int is_full_time=2;
 	public static final int emp_rate_per_hour = 20;
+	public static final int num_of_working_days = 2;
 
 	public static void main(String[] args) {
 		
@@ -15,9 +16,10 @@ public class EmployeeWageComputation {
 		
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		
+		for(int day = 0; day < num_of_working_days ; day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		
 		switch(empCheck) {
 		case is_part_time:
 			empHrs = 4;
@@ -29,8 +31,10 @@ public class EmployeeWageComputation {
 			empHrs = 0;
 		}
 		empWage = empHrs * emp_rate_per_hour;
+		totalEmpWage += empWage;
 		System.out.println("Emp Wage: " + empWage);
-	
+	}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
 
