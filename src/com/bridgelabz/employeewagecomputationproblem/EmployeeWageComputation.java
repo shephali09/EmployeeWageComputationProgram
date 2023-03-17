ec09ed154d8b615dc73fbf4de78da6c8e1310afd
shephali09 <hiremathshephali9@gmail.com>
2023-03-17 23:04:41 +0530

    Add: Calculate wages till a conditionof total working hours or days is reached for  a month

--- src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.employeewagecomputationproblem;
 
-/*Calculating Wages for a Month assuming 20
-  Working Days in a Month */
+/*Calculate Wages till a condition of total
+working hours or days is reached for
+a month - Assume 100 hours and 20 days */
  
 public class EmployeeWageComputation {
 	
@@ -9,16 +10,18 @@ public class EmployeeWageComputation {
 	public static final int is_full_time=2;
 	public static final int emp_rate_per_hour = 20;
 	public static final int num_of_working_days = 2;
+	public static final int max_hrs_in_month = 10; 
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program!");
 		
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 		
-		for(int day = 0; day < num_of_working_days ; day++) {
+		while(totalEmpHrs <= max_hrs_in_month  && totalWorkingDays <= num_of_working_days ) {
+		totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch(empCheck) {
 		case is_part_time:
@@ -30,10 +33,10 @@ public class EmployeeWageComputation {
 		default:
 			empHrs = 0;
 		}
-		empWage = empHrs * emp_rate_per_hour;
-		totalEmpWage += empWage;
-		System.out.println("Emp Wage: " + empWage);
+		totalEmpHrs += empHrs;
+		System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
 	}
+		int totalEmpWage = totalEmpHrs * emp_rate_per_hour;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
