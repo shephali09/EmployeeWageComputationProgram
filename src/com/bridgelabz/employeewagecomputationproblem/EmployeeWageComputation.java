10ce608aa4a93883a76e5510e24908015e49102d
shephali09 <hiremathshephali9@gmail.com>
2023-03-17 21:41:52 +0530

    Add: Add part time employee and wage

--- src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputation.java
@@ -1,20 +1,27 @@
 package com.bridgelabz.employeewagecomputationproblem;
 
+/* Add part time employee 
+   and wage */
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program!");
 		
-		int is_full_time=1;
+		int is_part_time=1;
+		int is_full_time=2;
 		int emp_rate_per_hour = 20;
+		
 		int empHrs = 0;
 		int empWage = 0;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == is_full_time) {
+		if(empCheck == is_part_time) {
+			empHrs = 4;
+		} else if(empCheck == is_full_time){
 			empHrs = 8;
-		} else {
+		}else {
 			empHrs = 0;
 		}
 		empWage = empHrs * emp_rate_per_hour;
